1ca44a1a199c0e1fca0d1b88629d8f123629aa36
sebudea <sebastian.aristizabalc@udea.edu.co>
2025-05-06 15:39:43 -0500

    pantalla inicial

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,4 @@
+class AppConstants {
+  static const String fullName = "Objetos Perdidos Lost UdeA";
+  static const String name = "Lost UdeA";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lostudea/constants/app_constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,149 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lost UdeA',
+      title: AppConstants.fullName,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Thhfhen, without quitting the app,
-        // try changing the seedColor in the colofgrScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes orfsd press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(AppConstants.name),
+          centerTitle: true,
+        ),
+        body: const LobbyScreen(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LobbyScreen extends StatefulWidget {
+  const LobbyScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LobbyScreen> createState() => _LobbyScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LobbyScreenState extends State<LobbyScreen> {
+  final _controller1 = TextEditingController();
+  final _controller2 = TextEditingController();
+  final _controller3 = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  bool showQuestion2 = false;
+  bool showQuestion3 = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller1.addListener(() {
+      if (_controller1.text.isNotEmpty && !showQuestion2) {
+        setState(() => showQuestion2 = true);
+      }
+    });
+
+    _controller2.addListener(() {
+      if (_controller2.text.isNotEmpty && !showQuestion3) {
+        setState(() => showQuestion3 = true);
+      }
     });
   }
 
   @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    _controller3.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "¿Encontraste un Objeto?",
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+                // Formulario progresivo
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        child: TextField(
+                          controller: _controller1,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "¿Cuál es el objeto?"),
+                        ),
+                      ),
+                      if (showQuestion2) ...[
+                        const SizedBox(height: 16),
+                        SizedBox(
+                          width: 300,
+                          child: TextField(
+                            controller: _controller2,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "¿Dónde lo encontraste?"),
+                          ),
+                        ),
+                      ],
+                      if (showQuestion3) ...[
+                        const SizedBox(height: 16),
+                        SizedBox(
+                          width: 300,
+                          child: TextField(
+                            controller: _controller3,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "¿Cuándo lo encontraste?"),
+                          ),
+                        ),
+                      ],
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+
+          // Línea divisoria
+          Container(
+            height: 1,
+            color: Colors.grey,
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+          ),
+
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "¿Perdiste un Objeto?",
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.g_mobiledata),
+                ),
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
